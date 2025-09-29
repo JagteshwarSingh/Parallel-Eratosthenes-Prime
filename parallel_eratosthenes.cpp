863684442a86eb0371966463e6cd74ff2ca7c308
Anish Mani <anish1mani@gmail.com>
2025-09-29 16:24:24 -0400

    Fixed issues with the intial serial calulation of primes. Also serpated chunks fo the function to allow for easier parallelization

--- parallel_eratosthenes.cpp
+++ parallel_eratosthenes.cpp
@@ -21,8 +21,7 @@ class word{
 
             const bool isPrime = (wordArray[wordArrayIndex] & mask) == 0;
 
-            std::cout << "currnum: " << curr_num << ", index: " << wordArrayIndex << ", mask: " << mask << ", isPrime: " << isPrime << std::endl;
-            //printf("currnum: %ld, index: %ld, mask: %ld, isprime: %i\n", curr_num, wordArrayIndex, mask);
+            //std::cout << "currnum: " << curr_num << ", index: " << wordArrayIndex << ", mask: " << mask << ", isPrime: " << isPrime << std::endl;
             
 
             return isPrime;
@@ -39,30 +38,49 @@ class word{
             return;
         }   
 
-        uint64_t bitwise_eratosthenes(uint64_t size) {
+        uint64_t bitwise_eratosthenes(uint64_t size, uint64_t lim) {
 		
             uint64_t count = 1; // 2 is a special case
                 
-            uint64_t lim = sqrt(size);
+            //uint64_t lim = sqrt(size);
             for (uint64_t i = 3; i <= lim; i += 2){
                 if (is_prime(i)) { //isprime(3)
                     count++;    //count = 2
-                    printf("count: %ld\n", count);
+                    //printf("count: %ld\n", count);
                     
                     for (uint64_t j = i * i; j <= size; j += 2 * i){
                         setnclear_word(j);
-                        printf("j: %ld\n", j);
+                        //printf("j: %ld\n", j);
                     }
                 }
             }
 
-            for (uint64_t i = (lim + 1) | 1; i <= size; i += 2)
-                if (is_prime(i)){
-                    count++;
-                    printf("i: %ld, count: %ld\n", i, count);
-                }
             return count;
         }
+
+        uint64_t count_primes(uint64_t start, uint64_t end, uint64_t count){
+
+            for (uint64_t i = (end + 1) | 1; i <= start; i += 2)
+                if (is_prime(i)){
+                    count++;
+                    //printf("i: %ld, count: %ld\n", i, count);
+                }
+
+            return count;
+        }
+
+        uint64_t serial_eratosthenes(uint64_t n){
+
+            uint64_t count;
+            uint64_t lim = sqrt(n);
+            count = bitwise_eratosthenes(n, lim);
+            count = count_primes(lim, n, count);
+
+
+            return count;
+        }
+
+
               
 };
 
@@ -92,23 +110,22 @@ int main(int _argc, char* _argv[]) {
 
     cout << "Finding primes up to " << n << " using " << num_threads << " threads." << endl;
 
-    simple_erastosthenes(n, num_threads);
+    //simple_erastosthenes(n, num_threads);
 
-
-    /*
-     * Use the following area for parallel and bitwise   
-     */
 
     word primes;
     
+
     primes.n = n;
     primes.numWords = (n / 64) + 1;
-    primes.wordArray = new uint64_t(primes.numWords);
+
+    primes.wordArray = new uint64_t[primes.numWords];
     for (uint64_t i = 0; i < primes.numWords; i++){
         primes.wordArray[i] = 0;
     }
 
-    uint64_t be_count = primes.bitwise_eratosthenes(n);
+    
+    uint64_t be_count = primes.serial_eratosthenes(n);
    
     printf("becount: %ld\n", be_count);
 
